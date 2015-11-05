510c748f567bfe717a2874c3fd62d09e9f5d30a2
Sofiane H <sofiane.hamadi.pro@gmail.com>
2015-11-05 12:05:57 +0100

    Interface des différent modele + Model Fee (a completer)

---
+++ models/Fee.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ */
+class Fee implements ModelInterface
+{
+  private id_fee;
+  private id_note;
+  private id_fee_type;
+  private date_fee;
+  private caption;
+  private amount;
+
+  function __construct(argument)
+  {
+    # code...
+  }
+
+  public function get() {
+
+  }
+
+  public function create($args) {
+
+  }
+
+  public function save() {
+
+  }
+}
+
+?>
---
+++ models/ModelInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Interface des différent modèle
+ */
+Interface ModelInterface
+{
+  public function get();
+  public function create($args);
+  public function save();
+}
+
+?>
