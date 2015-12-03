a3da6a6ff3d37595e6f60d11ae12740910bba7bc
Keyvan E <astaen@outlook.fr>
2015-12-03 18:15:14 +0100

    Fix slim (Slim doit forcément etre dans le root, voilà)

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ ini_set('xdebug.var_display_max_depth', 5);
 ini_set('xdebug.var_display_max_children', 999);
 ini_set('xdebug.var_display_max_data', 9999);
 
-require 'vendor/fpdf/fpdf.php';
-require 'vendor/Slim/Slim.php';
+require './vendor/fpdf/fpdf.php';
+require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 //importation des fonctions de
