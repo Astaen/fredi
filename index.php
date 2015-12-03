c3389c4f4f8d63151f3f7516faaa9fab6a79e22d
Keyvan E <astaen@outlook.fr>
2015-12-03 17:28:27 +0100

    Dossier slim: suppression du dossier parent contenant des tests inutiles)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('xdebug.var_display_max_children', 999);
 ini_set('xdebug.var_display_max_data', 9999);
 
 require 'vendor/fpdf/fpdf.php';
-require 'vendor/slim/Slim/Slim.php';
+require 'vendor/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 //importation des fonctions de
