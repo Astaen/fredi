6cf3f7d271850e913fc74642eb3d5a866ff3eadc
Sofiane H <sofiane.hamadi.pro@gmail.com>
2015-12-03 12:03:38 +0100

    Ajout d'un formulaire pour les nouveaux frais,  NON FINI

--- views/note/single.php
+++ views/note/single.php
@@ -1,5 +1,5 @@
 <?php
-// var_dump($note);
+ //var_dump($note);
 ?>
 
 <div class="card">
@@ -12,11 +12,11 @@
             <a class="btn-floating btn-large red">
                 <i class="large mdi-navigation-menu"></i>
             </a>
-            <ul>          
-                <li><a class="btn-floating blue tooltipped" alt="Ajouter un frais" data-position="top" data-delay="50" data-tooltip="Ajouter un frais"><i class="material-icons">add</i></a></li>
+            <ul>
+                <li><a class="btn-floating blue tooltipped modal-trigger" onclick="$('#add_fee').openModal();" alt="Ajouter un frais" data-position="top" data-delay="50" data-tooltip="Ajouter un frais"><i class="material-icons">add</i></a></li>
                 <li><a class="btn-floating green tooltipped" alt="Télécharger la fiche" data-position="top" data-delay="50" data-tooltip="Télécharger la fiche"><i class="material-icons">file_download</i></a></li>
             </ul>
-        </div>    
+        </div>
         <p class="flow-text"><?= $note->total . " €"; ?></p>
     </div>
 </div>
@@ -37,9 +37,39 @@
         		<td><?= $fee->caption; ?></td>
         		<td><?= $fee->amount . " €"; ?></td>
         		<td>
-    				<a href="/fee/<?= $fee->id_fee; ?>/edit" class="waves-effect waves-light btn blue"><i class="material-icons left">mode_edit</i>Editer</a> 
+    				<a href="/fee/<?= $fee->id_fee; ?>/edit" class="waves-effect waves-light btn blue"><i class="material-icons left">mode_edit</i>Editer</a>
         		</td>
             </tr>
         <?php endforeach; ?>
-    </tbody>		
+    </tbody>
 </table>
+
+  <!-- Modal Structure -->
+  <div id="add_fee" class="modal modal-fixed-footer">
+    <div class="modal-content">
+      <h4>Ajouter un nouveau frais</h4>
+      <form class="row" action="/note/<?= $note->id_note; ?>/add_fee" method="post">
+        <div class="input-field col s12">
+          <input placeholder="Burger King" id="caption" type="text" class="validate">
+          <label for="caption">Libelle</label>
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Montant en euro" id="amount" type="number" step="0.01" class="validate">
+          <label for="amount">Montant</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="creation_date" type="date" class="datepicker picker__input">
+        </div>
+      </form>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Envoyer</a>
+      <a href="#!" class="modal-action modal-close waves-effect waves-red btn-flat ">Annuler</a>
+    </div>
+  </div>
+<script type="text/javascript">
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+</script>
