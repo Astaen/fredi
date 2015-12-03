298dcdf2e48c8e2e059745954b72aa3dc9cec7e7
Keyvan E <astaen@outlook.fr>
2015-12-03 09:14:57 +0100

    Ajout de jquery en local

--- views/note/single.php
+++ views/note/single.php
@@ -1,7 +1,16 @@
 <?php
 var_dump($note);
 ?>
+
 <h2>Fiche #<?= $note->id_note; ?> - Année <?= $note->year; ?></h2>
+
+<div class="card">
+    <div class="card-content blue-text">
+        <span class="card-title">Dépenses</span>
+        <h2><?= $note->total . " €"; ?></h2>
+    </div>
+</div>
+
 <table class="highlight">
     <thead>
       <tr>
@@ -12,18 +21,18 @@ var_dump($note);
       </tr>
     </thead>
     <tbody>
-
-    		<tr>
-        			<td><?= $note->year; ?></td>
-        			<td><div class="chip <?= $note->id_note_state; ?>"><?= $note->libelle; ?></div></td>
-        			<td><?= $note->total . " €"; ?></td>
-        			<td>
-        				<a href="/note/<?= $note->id_note; ?>" class="waves-effect waves-light btn"><i class="material-icons left">pageview</i>Afficher</a>
-        				<?php if($note->id_note_state == 'open') { ?>
-        					<a href="/note/<?= $note->id_note; ?>/edit" class="waves-effect waves-light btn"><i class="material-icons left">mode_edit</i>Editer</a>
-        				<?php } ?>
-        			</td>
-    		</tr>
-
+        <?php foreach ($this->fees as $fee): ?>
+            <tr>
+        		<td><?= $note->year; ?></td>
+        		<td><div class="chip <?= $note->id_note_state; ?>"><?= $note->libelle; ?></div></td>
+        		<td><?= $note->total . " €"; ?></td>
+        		<td>
+        			<a href="/note/<?= $note->id_note; ?>" class="waves-effect waves-light btn"><i class="material-icons left">pageview</i>Afficher</a>
+        			<?php if($note->id_note_state == 'open') { ?>
+        				<a href="/note/<?= $note->id_note; ?>/edit" class="waves-effect waves-light btn"><i class="material-icons left">mode_edit</i>Editer</a>
+        			<?php } ?>
+        		</td>
+            </tr>
+        <?php endif; ?>
     </tbody>		
 </table>
